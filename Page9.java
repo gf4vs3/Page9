a2e1d8a6a696bac4bcad697170dad02fecc770f2
gf4vs3 <137363372+gf4vs3@users.noreply.github.com>
2024-02-07 14:43:51 -0500

    hahahahhaha

---
+++ Page9.java
@@ -0,0 +1,14 @@
+/*************************************
+ *Project:  Alexander Zalewski 
+ *Date:  2/6
+ *Program Name:  Page9.java
+**************************************/ 
+
+public class Page9
+{
+public static void main (String[ ] args)
+{
+    System.out.println("Hi"); //message to user
+    System.out.println("LabOneA"); //assignment
+}
+}
